0d002c6e3a8e3b7b2e5549928333a5ab0769ed0d
Adrien-S <73822547+Adrien-S@users.noreply.github.com>
2021-02-28 14:33:11 +0100

    Create SYROTNIK.js

---
+++ SYROTNIK.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#TextePresentation', {
+    strings: ["Bonjour et bienvenue sur mon CV !", "Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du 6 avril au 16 juillet"],
+    typeSpeed: 30,
+    smartBackspace: true,
+    loop: true
+
+  });
