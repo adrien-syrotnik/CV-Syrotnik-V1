a5e8ff1dff011e2717165febb92384590883a1bc
Adrien-S <73822547+Adrien-S@users.noreply.github.com>
2021-03-04 00:42:15 +0100

    Final ?

--- SYROTNIK.js
+++ SYROTNIK.js
@@ -1,4 +1,8 @@
-var typed = new Typed('#TextePresentation', {
+
+$('document').ready(function () {
+
+
+  var typed = new Typed('#TextePresentation', {
     strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>."],
     typeSpeed: 10,
     showCursor: false
@@ -6,19 +10,7 @@ var typed = new Typed('#TextePresentation', {
   });
 
 
-  /*anime({
-    targets: '.adri',
-    translateY: function(el, i) {
-      return 50 + (-1 * i);
-    },
-    scale: function(el, i, l) {
-      return (l - i) + .1;
-    },
-    rotate: function() { return anime.random(-1, 1); },
-    duration: 300,
-    //delay: function() { return anime.random(0, 400); },
-    direction: 'alternate',
-    loop: true
-  });*/
 
-  
+});
+
+
