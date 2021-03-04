0cb124869c9942b0154b310abc80b9a1a46a60f0
Adrien-S <73822547+Adrien-S@users.noreply.github.com>
2021-03-04 01:48:00 +0100

    FINAL

--- SYROTNIK.js
+++ SYROTNIK.js
@@ -3,14 +3,15 @@ $('document').ready(function () {
 
 
   var typed = new Typed('#TextePresentation', {
-    strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>."],
+    strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>.</br></br>Adorant la programmation et plus motivé que jamais, j'aimerais trouver un stage dans le domaine du web ou logiciel, même si cependant le domaine des jeux-vidéo m'est encore plus intéressant."],
     typeSpeed: 10,
     showCursor: false
-
   });
 
 
+  AOS.init();
 
 });
+
 
 
