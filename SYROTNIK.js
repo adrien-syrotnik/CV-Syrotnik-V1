45d5c9ea6cd79fbd029c0cfe7b06a2cffa78b81b
Adrien-S <73822547+Adrien-S@users.noreply.github.com>
2021-03-20 14:36:15 +0100

    PWA

--- SYROTNIK.js
+++ SYROTNIK.js
@@ -3,7 +3,7 @@ $('document').ready(function () {
 
 
   var typed = new Typed('#TextePresentation', {
-    strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>.</br></br>Adorant la programmation et plus motivé que jamais, j'aimerais trouver un stage dans le domaine du web ou logiciel, même si le domaine du jeu vidéo reste le plus intéressant pour moi."],
+    strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>.</br></br>Adorant la programmation et plus motivé que jamais, j'aimerais trouver un stage dans le domaine du web ou du logiciel."],
     typeSpeed: 10,
     showCursor: false
   });
---
+++ sw.js
@@ -0,0 +1,50 @@
+/*importScripts('sw-toolbox.js');
+toolbox.precache(['index.html', 'SYROTNIK.css', 'SYROTNIK.js']);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});*/
+const staticCacheName = 'site-static-v1';//
+const dynamicCache = 'site-dynamic-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/SYROTNIK.css',
+    '/SYROTNIK.js',
+    '/Image/CV Adrien Syrotnik.pdf'
+];
+
+
+
+self.addEventListener('install', evt => {
+    //console.log('installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            cache.addAll(assets);
+        })
+        );
+});
+
+self.addEventListener('activate', evt => {
+    //console.log('activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+                )
+        })
+    );
+});
+
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);/*.then(fetchRes =>{
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });*/
+        })
+    );
+});
