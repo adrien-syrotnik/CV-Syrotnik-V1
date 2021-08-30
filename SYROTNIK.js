6752dd8022d4813e7ce6b5aceec1f59146034b4b
Adrien-S <adrien.syrotnik@viacesi.fr>
2021-08-30 09:58:28 +0200

    update avant refonte

--- SYROTNIK.js
+++ SYROTNIK.js
@@ -1,17 +1,21 @@
-
-$('document').ready(function () {
-
-
-  var typed = new Typed('#TextePresentation', {
-    strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>6 avril</em> au <em>16 juillet</em>.</br></br>Adorant la programmation et plus motivé que jamais, j'aimerais trouver un stage dans le domaine du web ou du logiciel."],
-    typeSpeed: 10,
-    showCursor: false
-  });
+$('document').ready(function() {
 
 
-  AOS.init();
+    var typed = new Typed('#TextePresentation', {
+        strings: ["Je m'appelle Adrien SYROTNIK et je suis à la recherche d'un stage se déroulant du <em>3 janvier</em> au <em>8 avril</em> 2022.</br></br>Adorant la programmation et plus motivé que jamais, j'aimerais principalement trouver un stage sur le logiciel Unity, même si le domaine du web m'intéresse également."],
+        typeSpeed: 10,
+        showCursor: false
+    });
+
+
+    var typed = new Typed('#Points', {
+        strings: ["..."],
+        typeSpeed: 300,
+        showCursor: false,
+        loop: true
+    });
+
+
+    AOS.init();
 
 });
-
-
-
